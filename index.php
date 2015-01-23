b3e48701e474c5183b83dcd8b22faf9e6130defa
Juzet Arana <arana.juzet@gmail.com>
2015-01-23 10:45:02 -0800

    commented video 2

--- index.php
+++ index.php
@@ -62,3 +62,4 @@
 	</body>
 </html>
 <!-- I just moved the code from the other index file to this file so that the template of the melon js file will appear in my php file -->
+<!-- This is the code provided to me from the person who started to make this map template -->
