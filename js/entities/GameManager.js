e1a18209737e32066d12a26c37042e932fc49676
Juzet Arana <arana.juzet@gmail.com>
2015-03-12 23:35:32 -0700

    finished 47 one error about an ancestor

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -98,11 +98,39 @@ game.SpendGold = Object.extend ({
 		this.lastBuy = new Date().getTime();
 		this.paused = false;
 		this.alwaysUpdate = true;	
+		this.updateWhenPaused = true;
+		this.buying = false;
 	},
 
 	update: function() {
+		this.now = new Date().getTime();
+		if (me.input.isKeyPressed("buy") && this.now-this.lastBuy >= 1000 ) {
+			this.lastBuy = this.now;
+			if (!this.buying) {
+				this.startBuying();
+			}
+			else {
+				this.stopBuying();
+			}
+
+		}
 		return true;
 	}, 
-
+	startBuying: function () {
+		this.buying = true;
+		me.state.pause(me.state.PLAY);
+		game.data.pausePos = me.game.viewport.localToWorld(0, 0);
+		game.data.buyScreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage('gold-screen'));
+		game.data.buyScreen.updateWhenPaused = true;
+		game.data.buyScreen.setOpacity(0.8);
+		me.game.world.addChild(game.data.buyscreen, 34);
+		game.data.player.body.setVelocity(0, 0);
+	},
+	stopBuying: function () {
+		this.buying = false;
+		me.state.resume(me.state.PLAY);
+		game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
+		me.game.world.removeChild(game.data.buyscreen);
+	}
 });
 
