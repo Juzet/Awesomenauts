073372b4d73357a4427d2f4fad47cdfe04dc3fdb
Juzet Arana <arana.juzet@gmail.com>
2015-03-25 11:17:20 -0700

    coded and commented 50

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -116,6 +116,8 @@ game.SpendGold = Object.extend ({
 			}
 
 		}
+		this.checkBuyKeys();
+
 		return true;
 	}, 
 	startBuying: function () {
@@ -180,6 +182,97 @@ game.SpendGold = Object.extend ({
 		me.game.world.removeChild(game.data.buytext);
 		// these are stopping the functions
 		// this is unbinding the functions for when you are not buying
+	},
+
+	checkBuyKeys: function() {
+		if (me.input.isKeyPressed("F1")) {
+			if(this.checkCost(1)){
+				this.makePurchase(1);
+			}
+		}
+		else if (me.input.isKeyPressed("F2")) {
+			if(this.checkCost(2)){
+				this.makePurchase(2);
+			}
+		}
+		else if (me.input.isKeyPressed("F3")) {
+			if(this.checkCost(3)){
+				this.makePurchase(3);
+			}
+		}
+		else if (me.input.isKeyPressed("F4")) {
+			if(this.checkCost(4)){
+				this.makePurchase(4);
+			}
+		}
+		else if (me.input.isKeyPressed("F5")) {
+			if(this.checkCost(5)){
+				this.makePurchase(5);
+			}
+		}
+		else if (me.input.isKeyPressed("F6")) {
+			if(this.checkCost(6)){
+				this.makePurchase(6);
+			}
+		}
+	}, 
+	
+	checkCost: function(skill) {
+		if(skill===1 && (game.data.gold >= ((game.data.skill1 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 10 for the cost
+			return true;
+		}
+		else if(skill===2 && (game.data.gold >= ((game.data.skill2 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 0 for the cost
+			return true;
+		}
+		else if(skill===3 && (game.data.gold >= ((game.data.skill3 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 10 for the cost
+			return true;
+		}
+		else if(skill===4 && (game.data.gold >= ((game.data.ability1 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 10 for the cost
+			return true;
+		}
+		else if(skill===5 && (game.data.gold >= ((game.data.ability2 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 10 for the cost
+			return true;
+		}
+		else if(skill===6 && (game.data.gold >= ((game.data.ability3 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 10 for the cost
+			return true;
+		}
+		else {
+			return false;
+		}
+	},
+	
+	makePurchase: function(skill) {
+		if(skill===1) {
+			game.data.gold -= ((game.data.skill1 +1)*10);
+			game.data.skill1 += 1;
+			game.data.player.attack += 1; 
+		}
+		else if(skill===2) {
+			game.data.gold -= ((game.data.skill2 +1)*10);
+			game.data.skill2 += 1;
+		}
+		else if(skill===3) {
+			game.data.gold -= ((game.data.skill3 +1)*10);
+			game.data.skill3 += 1;
+		}
+		if(skill===4) {
+			game.data.gold -= ((game.data.ability1 +1)*10);
+			game.data.ability1 += 1;
+		}
+		if(skill===5) {
+			game.data.gold -= ((game.data.ability2 +1)*10);
+			game.data.ability2 += 1;
+		}
+		if(skill===6) {
+			game.data.gold -= ((game.data.ability3 +1)*10);
+			game.data.ability3 += 1;
+		}
 	}
 });
 
